5406ac662df8aeef5107f432e0fad4e4ccd76577
Lydia <lydiadoula@gmail.com>
2023-06-27 23:24:55 +0000

    some stuff

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 function convertDistance() {
-    // Get the input value and selected conversion direction
-    const distanceInput = document.getElementById("distanceInput").value;
-    const conversionDirection = document.querySelector('input[name="conversion"]:checked').value;
-  
-    // Convert the distance based on the selected conversion direction
-    let result;
-    if (conversionDirection === "mile") {
-      result = distanceInput * 1.60934;
-      document.getElementById("result").innerHTML = distanceInput + " miles is equal to " + result.toFixed(2) + " kilometers.";
-    } else if (conversionDirection === "km") {
-      result = distanceInput / 1.60934;
-      document.getElementById("result").innerHTML = distanceInput + " kilometers is equal to " + result.toFixed(2) + " miles.";
-    }
+  // Get the input value and selected conversion direction
+  const distanceInput = document.getElementById("distanceInput").value;
+  const conversionDirection = document.querySelector('input[name="conversion"]:checked').value;
+
+  // Convert the distance based on the selected conversion direction
+  let result;
+  if (conversionDirection === "mile") {
+    result = distanceInput * 1.60934;
+    document.getElementById("distanceResult").innerHTML = distanceInput + " miles is equal to " + result.toFixed(2) + " kilometers.";
+  } else if (conversionDirection === "km") {
+    result = distanceInput / 1.60934;
+    document.getElementById("distanceResult").innerHTML = distanceInput + " kilometers is equal to " + result.toFixed(2) + " miles.";
   }
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    const convertButton = document.querySelector("button");
-    convertButton.addEventListener("click", convertDistance);
-  });
+}
+
+function convertElevation() {
+  // Get the input value and selected conversion direction
+  const feetInput = document.getElementById("feetInput").value;
+  const conversionDirection = document.querySelector('input[name="conversion"]:checked').value;
+
+  // Convert the elevation based on the selected conversion direction
+  let result;
+  if (conversionDirection === "feet") {
+    result = feetInput * 0.3048;
+    document.getElementById("elevationResult").innerHTML = feetInput + " feet is equal to " + result.toFixed(2) + " meters.";
+  } else if (conversionDirection === "meter") {
+    result = feetInput / 0.3048;
+    document.getElementById("elevationResult").innerHTML = feetInput + " meters is equal to " + result.toFixed(2) + " feet.";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const distanceConvertButton = document.querySelector(".converter-btn");
+  distanceConvertButton.addEventListener("click", convertDistance);
+
+  const elevationConvertButton = document.querySelector(".elevation-btn");
+  elevationConvertButton.addEventListener("click", convertElevation);
+});
+
+
+
+
+
+
